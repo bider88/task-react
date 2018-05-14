dce25ede7724c1bb9441a3cd9c29c77a58555465
Didier <bider88@gmail.com>
2018-05-14 13:06:23 -0500

    finalizado app Todo o Task

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ class App extends Component {
         this.handleAddTask = this.handleAddTask.bind(this)
     }
 
+    deleteTask(index) {
+        this.setState({
+            todo: this.state.todo.filter((e, i) => {
+                return i !== index
+            })
+        })
+    }
+
     handleAddTask(task) {
         this.setState({
             todo: [...this.state.todo, task]
@@ -26,16 +34,18 @@ class App extends Component {
 
         const todo = this.state.todo.map((t, i) => {
             return (
-                <div className="card" key={i}>
-                    <div className="card-header">
-                        {t.title}
-                    </div>
-                    <div className="card-body ">
-                        <h6 className="card-subtitle mb-2 text-muted" >{t.responsable}</h6>
-                        <p>Prioridad <Priority priority={t.priority} /></p>
-                        <p className="card-text">{t.description}</p>
-                        <div className="text-center">
-                            <a href="" className="btn btn-danger">Eliminar</a>
+                <div className="col-md-4" key={i}>
+                    <div className="card m-2">
+                        <div className="card-header">
+                            {t.title}
+                        </div>
+                        <div className="card-body ">
+                            <h6 className="card-subtitle mb-2 text-muted" >{t.responsable}</h6>
+                            <p>Prioridad <Priority priority={t.priority} /></p>
+                            <p className="card-text">{t.description}</p>
+                            <div className="text-center">
+                                <button className="btn btn-danger" onClick={this.deleteTask.bind(this, i)}>Eliminar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -45,22 +55,22 @@ class App extends Component {
         function Priority(props) {
             const priority = props.priority;
             if (priority) {
-              return <i className="fa fa-star-o"></i>;
+                return <i className="fa fa-star"></i>;
             }
-            return <i className="fa fa-star"></i>;
-          }
+            return <i className="fa fa-star-o"></i>;
+        }
 
         return (
             <div className="App">
-                <Navigation title="Task React" count={ todo.length  } />
+                <Navigation title="Task React" count={todo.length} />
                 <div className="container mt-5">
                     <div className="row">
                         <div className="col-md-3">
-                            <TodoForm  onAddTask={this.handleAddTask} />
+                            <TodoForm onAddTask={this.handleAddTask} />
                         </div>
                         <div className="col-md-9">
-                            <div className="card-columns">
-                                { todo }    
+                            <div className="row">
+                                {todo}
                             </div>
                         </div>
                     </div>
